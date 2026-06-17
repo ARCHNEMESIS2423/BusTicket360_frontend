56ea4c0ea562a369ac51e2c84cff9b5fd119c30b
ARCHNEMESIS2423 <jmkolanguzi@gmail.com>
2026-06-17 21:18:07 +0300

    fix: update base URL asset paths

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: "https://github.com/ARCHNEMESIS2423/BusTicket360_frontend",
+  base: "/BusTicket360_frontend/",
 })
